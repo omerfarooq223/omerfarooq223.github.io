60b5edcbbaae42f24975c5db39247129112cd4e5
omerfarooq223 <momerfarooq223@gmail.com>
2026-04-14 04:04:11 +0500

    fix: point chatbot to absolute Vercel URL

--- chatbot-widget.js
+++ chatbot-widget.js
@@ -25,7 +25,7 @@
     }
 
     try {
-      const response = await fetch('/api/chat', {
+      const response = await fetch('https://omerfarooq223-github-io-git-main-omerfarooq223s-projects.vercel.app/api/chat', {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify({ message: query })
